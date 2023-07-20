550df8a4897f39824412defa7a071deba3b4b99f
M-Dablju <mariusz.a.wojcik@gmail.com>
2023-07-20 12:46:29 +0200

    Updated the code to use the database.

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
-
-// Global variables to store information about envelopes and total budget
-let envelopes = [];
-let totalBudget = 0;
+const pool = require('./db');
 
 app.use(express.json()); // Middleware to parse JSON requests
 
 // Endpoint to generate individual budget envelopes
-app.post('/envelopes', (req, res) => {
+app.post('/envelopes', async (req, res) => {
   const { title, budget } = req.body;
 
   // Validate request body
@@ -16,142 +13,170 @@ app.post('/envelopes', (req, res) => {
     return res.status(400).json({ error: 'Invalid request. Title and budget are required and budget must be a positive number.' });
   }
 
-  // Create a new envelope object
-  const envelope = {
-    id: envelopes.length + 1,
-    title,
-    budget,
-    balance: budget, // Initially, balance is the same as the budget
-  };
+  try {
+    const result = await pool.query('INSERT INTO envelopes (title, budget, balance) VALUES ($1, $2, $2) RETURNING *', [title, budget]);
+    const newEnvelope = result.rows[0];
 
-  // Add the envelope to the envelopes array
-  envelopes.push(envelope);
+    const totalBudgetResult = await pool.query('SELECT SUM(budget) AS total_budget FROM envelopes');
+    const totalBudget = totalBudgetResult.rows[0].total_budget;
+    await pool.query('UPDATE budget SET total_budget = $1', [totalBudget]);
 
-  // Update the total budget
-  totalBudget += budget;
-
-  // Return the newly created envelope as the response
-  res.status(201).json(envelope);
+    res.status(201).json(newEnvelope);
+  } catch (error) {
+    console.error('Error creating envelope:', error);
+    res.status(500).json({ error: 'An error occurred while generating the envelope.' });
+  }
 });
 
 // Endpoint to retrieve all envelopes
-app.get('/envelopes', (req, res) => {
+app.get('/envelopes', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM envelopes');
+    const envelopes = result.rows;
     res.json(envelopes);
+  } catch (error) {
+    console.error('Error retrieving envelopes:', error);
+    res.status(500).json({ error: 'An error occurred while retrieving envelopes.' });
+  }
 });
 
 // Endpoint to retrieve a specific envelope
-app.get('/envelopes/:id', (req, res) => {
+app.get('/envelopes/:id', async (req, res) => {
   const id = +req.params.id;
 
-  // Find the envelope with the corresponding ID
-  const envelope = envelopes.find((env) => env.id === id);
+  try {
+    const result = await pool.query('SELECT * FROM envelopes WHERE id = $1', [id]);
+    const envelope = result.rows[0];
 
-  // Check if the envelope exists
-  if (!envelope) {
-    return res.status(404).json({ error: 'Envelope not found.' });
+    if (!envelope) {
+      return res.status(404).json({ error: 'Envelope not found.' });
+    }
+
+    res.json(envelope);
+  } catch (error) {
+    console.error('Error retrieving envelope:', error);
+    res.status(500).json({ error: 'An error occurred while retrieving the envelope.' });
   }
-
-  // Return the envelope as the response
-  res.json(envelope);
 });
 
 // Endpoint to update specific envelopes
-app.put('/envelopes/:id', (req, res) => {
+app.put('/envelopes/:id', async (req, res) => {
   const id = +req.params.id;
   const { amount, budget } = req.body;
 
-  // Find the envelope with the corresponding ID
-  const envelope = envelopes.find((env) => env.id === id);
+  try {
+    const result = await pool.query('SELECT * FROM envelopes WHERE id = $1', [id]);
+    const envelope = result.rows[0];
 
-  // Check if the envelope exists
-  if (!envelope) {
-    return res.status(404).json({ error: 'Envelope not found.' });
+    if (!envelope) {
+      return res.status(404).json({ error: 'Envelope not found.' });
+    }
+
+    if (typeof amount === 'number' && amount !== 0) {
+      envelope.balance -= amount;
+
+      const totalBudgetResult = await pool.query('SELECT total_budget FROM budget');
+      const currentTotalBudget = totalBudgetResult.rows[0].total_budget;
+      await pool.query('UPDATE budget SET total_budget = $1', [currentTotalBudget - amount]);
+    }
+
+    if (typeof budget === 'number' && budget >= 0) {
+      envelope.budget = budget;
+      await pool.query('UPDATE envelopes SET budget = $1 WHERE id = $2', [budget, id]);
+    } else if (typeof budget !== 'undefined') {
+      return res.status(400).json({ error: 'Invalid budget. Budget must be a positive number.' });
+    }
+
+    res.json(envelope);
+  } catch (error) {
+    console.error('Error updating envelope:', error);
+    res.status(500).json({ error: 'An error occurred while updating the envelope.' });
   }
-
-  // Update the envelope's properties based on the request body
-  if (typeof amount === 'number' && amount !== 0) {
-    // Update the envelope balance based on the extracted amount
-    envelope.balance -= amount;
-
-    // Update the total budget accordingly
-    totalBudget -= amount;
-  }
-
-  if (typeof budget === 'number' && budget >= 0) {
-    // Update the envelope budget if provided in the request body
-    envelope.budget = budget;
-  }
-
-  // Return the updated envelope as the response
-  res.json(envelope);
 });
 
-// Endpoint to delete specific envelopes (DELETE request)
-app.delete('/envelopes/:id', (req, res) => {
+// Endpoint to delete specific envelopes 
+app.delete('/envelopes/:id', async (req, res) => {
   const id = +req.params.id;
 
-  // Find the envelope with the corresponding ID
-  const envelopeToDelete = envelopes.find((env) => env.id === id);
+  try {
+    const result = await pool.query('SELECT * FROM envelopes WHERE id = $1', [id]);
+    const envelope = result.rows[0];
 
-  // Check if the envelope exists
-  if (!envelopeToDelete) {
-    return res.status(404).json({ error: 'Envelope not found.' });
+    if (!envelope) {
+      return res.status(404).json({ error: 'Envelope not found.' });
+    }
+
+    await pool.query('DELETE FROM envelopes WHERE id = $1', [id]);
+
+    const totalBudgetResult = await pool.query('SELECT SUM(budget) AS total_budget FROM envelopes');
+    const currentTotalBudget = totalBudgetResult.rows[0].total_budget;
+    await pool.query('UPDATE budget SET total_budget = $1', [currentTotalBudget - envelope.balance]);
+
+    const message = `Envelope "${envelope.title}" was deleted.`;
+    res.json({ message });
+  } catch (error) {
+    console.error('Error deleting envelope:', error);
+    res.status(500).json({ error: 'An error occurred while deleting the envelope.' });
   }
-
-  // Create a new array without the deleted envelope using the filter method
-  envelopes = envelopes.filter((env) => env.id !== id);
-
-  // Update the total budget by subtracting the deleted envelope's balance
-  totalBudget -= envelopeToDelete.balance;
-
-  // Send a message indicating that the envelope was deleted
-  const message = `Envelope "${envelopeToDelete.title}" was deleted.`;
-  res.send(JSON.stringify({ message }, null, 2).replace(/\\n|\\r|\\|\\/g, '')); // removing slashes from the response XD
 });
 
-// Endpoint to transfer budgets between envelopes (POST request)
-app.post('/envelopes/transfer/:from/:to', (req, res) => {
+// Endpoint to transfer budgets between envelopes 
+app.post('/envelopes/transfer/:from/:to', async (req, res) => {
   const fromId = +req.params.from;
   const toId = +req.params.to;
   const amount = +req.body.amount;
 
-  // Find the 'from' envelope with the corresponding ID
-  const fromEnvelope = envelopes.find((env) => env.id === fromId);
+  try {
+    const fromEnvelopeResult = await pool.query('SELECT * FROM envelopes WHERE id = $1', [fromId]);
+    const fromEnvelope = fromEnvelopeResult.rows[0];
 
-  // Check if the 'from' envelope exists
-  if (!fromEnvelope) {
-    return res.status(404).json({ error: 'Source envelope not found.' });
+    if (!fromEnvelope) {
+      return res.status(404).json({ error: 'Source envelope not found.' });
+    }
+
+    const toEnvelopeResult = await pool.query('SELECT * FROM envelopes WHERE id = $1', [toId]);
+    const toEnvelope = toEnvelopeResult.rows[0];
+
+    if (!toEnvelope) {
+      return res.status(404).json({ error: 'Destination envelope not found.' });
+    }
+
+    if (isNaN(amount) || typeof amount !== 'number' || amount <= 0) {
+      return res.status(400).json({ error: 'Invalid amount provided. Amount must be a valid number greater than zero.' });
+    }
+
+    if (fromEnvelope.balance < amount) {
+      return res.status(400).json({ error: 'Insufficient balance in the source envelope.' });
+    }
+
+    const fromBalance = Number(fromEnvelope.balance);
+    const toBalance = Number(toEnvelope.balance);
+
+    fromEnvelope.balance = fromBalance - amount;
+    toEnvelope.balance = toBalance + amount;
+
+    const newFromBalance = Number(fromEnvelope.balance);
+    const newToBalance = Number(toEnvelope.balance);
+
+    await pool.query('UPDATE envelopes SET balance = $1 WHERE id = $2', [newFromBalance, fromId]);
+    await pool.query('UPDATE envelopes SET balance = $1 WHERE id = $2', [newToBalance, toId]);
+
+    res.json({ fromEnvelope, toEnvelope });
+  } catch (error) {
+    console.error('Error transferring budget:', error);
+    res.status(500).json({ error: 'An error occurred while transferring the budget.' });
   }
-
-  // Find the 'to' envelope with the corresponding ID
-  const toEnvelope = envelopes.find((env) => env.id === toId);
-
-  // Check if the 'to' envelope exists
-  if (!toEnvelope) {
-    return res.status(404).json({ error: 'Destination envelope not found.' });
-  }
-
-  // Check if the amount is a valid number greater than zero
-  if (typeof amount !== 'number' || amount <= 0) {
-    return res.status(400).json({ error: 'Invalid amount provided.' });
-  }
-
-  // Check if the 'from' envelope has sufficient balance for the transfer
-  if (fromEnvelope.balance < amount) {
-    return res.status(400).json({ error: 'Insufficient balance in the source envelope.' });
-  }
-
-  // Perform the budget transfer
-  fromEnvelope.balance -= amount;
-  toEnvelope.balance += amount;
-
-  // Return the updated envelopes as the response (optional)
-  res.json({ fromEnvelope, toEnvelope });
 });
 
-// Root route to display the total budget
-app.get('/', (req, res) => {
-  res.send(`Total Budget: ${totalBudget}€`);
+app.get('/', async (req, res) => {
+  try {
+    const totalBudgetResult = await pool.query('SELECT total_budget FROM budget');
+    const totalBudget = totalBudgetResult.rows[0].total_budget;
+    res.send(`Total Budget: ${totalBudget}€`);
+  } catch (error) {
+    console.error('Error retrieving total budget:', error);
+    res.status(500).json({ error: 'An error occurred while retrieving the total budget.' });
+  }
 });
 
 const port = 3000;
